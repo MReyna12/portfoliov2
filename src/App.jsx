c078a2adb1fb91ec688cf168140cc283d76cd1ed
Michael <MReyna12@live.com>
2022-08-12 12:16:52 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import "bulma/css/bulma.min.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
